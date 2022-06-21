57654df423d485ecc2dd40983d45d773c4ec4b65
Wellyson-Lopes <107424893+Wellyson-Lopes@users.noreply.github.com>
2022-06-21 19:21:49 -0300

    ATUALIZAÇÃO

--- insercao_Banco.py
+++ insercao_Banco.py
@@ -1,6 +1,7 @@
 import sqlite3
 from sqlite3 import Error
 
+#CONEXÃO COM BANCO DE DADOS
 def conexaoBanco():
     caminho = "C:\\Users\\Wellyson Lopes\\Documents\\script_vacina\\Script_vacina\\banco_vacinas.db"
     con = None
@@ -9,4 +10,19 @@ def conexaoBanco():
     except Error as ex:
         print(ex)
     return con
+vcon = conexaoBanco()
 
+#CRIAÇÃO DE TABELA
+vsql="""CREATE TABLE tipo_vacinas(id INTEGER PRIMARY KEY AUTOINCREMENT, vacina VARCHAR(30), periodo INTEGER, dose INTEGER);"""
+
+def criartabela(conexao,sql):
+    try:
+        cursor = conexao.cursor()
+        cursor.execute(sql)
+        print("Tabela Criada com Sucesso!")
+    except Error as ex:
+        print(ex)
+
+criartabela(vcon,vsql)
+
+vcon.close()
