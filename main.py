862fe3c8be81ea18160b13736e09c7d2444bbf11
Wellyson-Lopes <107424893+Wellyson-Lopes@users.noreply.github.com>
2022-06-21 23:09:21 -0300

    atualização integrando script com db

--- main.py
+++ main.py
@@ -2,22 +2,33 @@ import datetime
 from datetime import timedelta, date
 import schedule
 import time
+import sqlite3
+from sqlite3 import Error
 
-usuario = input("Digite o nome do usuario: ")
+#CONEXÃO COM BANCO DE DADOS
+def conexaoBanco():
+    caminho = "C:\\Users\\Wellyson Lopes\\Documents\\script_vacina\\Script_vacina\\banco_vacinas.db"
+    con = None
+    try:
+        con = sqlite3.connect(caminho)
+    except Error as ex:
+        print(ex)
+    return con
+vcon = conexaoBanco()
+#input de dados pessoais
 
-vacina = input(f"{usuario},  selecione o tip ode vacina,'Penta', 'Rotavirus': ")
-
-if vacina == "Penta" or vacina == "penta":
-    vacina = vacina
-    periodo = 60
-
-elif vacina == "Rotavirus" or vacina == "rotavirus":
-    vacina = vacina
-    periodo = 50
+usuario_resposavel = input("Escreva o seu nome: ")
+filho = input(f"Olá {usuario_resposavel} tudo bem?, Escreva o nome de seu Filho(a)")
+sexo = input(f"{usuario_resposavel} você é o Pai ou a mãe de {filho}, escreva [P] para Pai ou [M] para Mãe: ")
+if sexo == "P" or sexo == "p":
+    sexo = "Papai"
 else:
-    print("as informações digitadas são inválidas, por favor escreva 'Penta' ou 'Rotavirus': ")
+    sexo = "Mamãe"
+
+tipo = input(f"Olá {sexo} {usuario_resposavel},  escreva o tipo de vacina que {filho} recebu,[BCG], [hepatiteB], [DTP],[tetravalente] ou [VOP]: ")
 
 
+# input e comandos refentes as datas de vacinação
 def data_vacina_inteiro(pergunta):
     data_vacina = input(pergunta)
     if data_vacina.isdigit():
@@ -25,7 +36,7 @@ def data_vacina_inteiro(pergunta):
         data = int(data)
     else:
         data = None
-        print(f"{usuario}, vcoê digitou um valor iválido. Tente novamente!!!")
+        print(f"{usuario_resposavel}, vcoê digitou um valor iválido. Tente novamente!!!")
     return data
 
 
@@ -41,13 +52,40 @@ ano = None
 while ano == None:
     ano = data_vacina_inteiro("Digite o ano da sua vacina: ")
 
-data_vacina1 = datetime.date(ano, mes, dia)
+data_vacina = datetime.date(ano, mes, dia)
 
-print(
-    f"{usuario}, você selecionou a vacina {vacina}, a data de sua vacina foi {data_vacina1} e o ciclo é de {periodo} dias")
-data_final = data_vacina1 + timedelta(days=periodo)
 
-print(f"sua proxima vacina {vacina} será no dia: {data_final}")
+
+#conulta de dados para colocar dentro de variaveis com tuplas
+def consulta(conexao,sql):
+    cursor = conexao.cursor()
+    cursor.execute(sql)
+    resultado = cursor.fetchall()
+    return resultado
+
+vsql = "SELECT * FROM tipo_vacinas WHERE vacina = '"+tipo+"'"
+res = consulta(vcon,vsql)
+for r in res:
+    print(f"{sexo} {usuario_resposavel}, {filho} tomou a vacina {tipo}")
+
+#esta condição está associada ao input do tipo da vacina na linha 28
+periodo = 0
+if tipo == "BCG":
+    periodo = r[2]
+elif tipo == "hepatiteB":
+    periodo = r[2] * 2
+elif tipo == "DTP":
+    periodo = 0
+elif tipo == "tetravalente":
+    periodo = r[2] * 2
+elif tipo == "VOP":
+    periodo = r[2] * 2
+periodo = int(periodo)
+
+print(f"{sexo} {usuario_resposavel}, a data da vacina de {filho} foi {data_vacina} e o ciclo é de {periodo} dias")
+data_final = data_vacina + timedelta(days=periodo)
+
+print(f"A proxima dose da vacina {tipo} será no dia: {data_final}")
 
 hoje = datetime.date.today()
 data_vacina_select = data_final - hoje
@@ -55,7 +93,7 @@ data_vacina_select = data_final - hoje
 
 
 def reepetirTarefa():
-    print(f"Faltam {data_vacina_select} para sua proxima Vacina, {vacina}")
+    print(f"{sexo} {usuario_resposavel} Faltam {data_vacina_select} para {filho} tomar sua proxima dose da Vacina, {tipo}")
 
 
 schedule.every(10).seconds.do(reepetirTarefa)
@@ -66,4 +104,3 @@ while contador <= 50:
     schedule.run_pending()
     time.sleep(1)
     contador += 1
-    
