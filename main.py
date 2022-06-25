7b5b7a6be94b44e19159895cfb126ef29e86919e
Fernando Shayani <shayani@gmail.com>
2022-06-24 22:37:53 -0300

    Cria arquivo de funções

--- main.py
+++ main.py
@@ -2,18 +2,8 @@ import datetime
 from datetime import timedelta, date
 import schedule
 import time
-import sqlite3
-from sqlite3 import Error
+from funcoes import *
 
-#CONEXÃO COM BANCO DE DADOS
-def conexaoBanco():
-    caminho = "./banco_vacinas.db"
-    con = None
-    try:
-        con = sqlite3.connect(caminho)
-    except Error as ex:
-        print(ex)
-    return con
 vcon = conexaoBanco()
 #input de dados pessoais
 
