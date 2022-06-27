807ede1f6959af10efab624b68267c7d54dd5e44
Wellyson-Lopes <107424893+Wellyson-Lopes@users.noreply.github.com>
2022-06-27 12:47:46 -0300

    atualizações 27/06/2022

--- criacao_tabela.py
+++ criacao_tabela.py
@@ -2,7 +2,33 @@ import conexao_banco
 from conexao_banco import *
 
 #CRIAÇÃO DE TABELA
-vsql="""CREATE TABLE IF NOT EXISTS tipo_vacinas(id INTEGER PRIMARY KEY AUTOINCREMENT, vacina VARCHAR(30), periodo INTEGER, dose INTEGER);"""
+vsql="CREATE TABLE IF NOT EXISTS tipo_vacinas(id INTEGER PRIMARY KEY AUTOINCREMENT, vacina VARCHAR(30), periodo INTEGER, dose INTEGER);"
+
+def criartabela(conexao,sql):
+    try:
+        cursor = conexao.cursor()
+        cursor.execute(sql)
+        print("Tabela Criada com Sucesso!")
+    except Error as ex:
+        print(ex)
+
+criartabela(vcon,vsql)
+
+
+vsql="CREATE TABLE IF NOT EXISTS usuarios(id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR(30), filho VARCHAR(30));"
+
+def criartabela(conexao,sql):
+    try:
+        cursor = conexao.cursor()
+        cursor.execute(sql)
+        print("Tabela Criada com Sucesso!")
+    except Error as ex:
+        print(ex)
+
+criartabela(vcon,vsql)
+
+
+vsql="CREATE TABLE IF NOT EXISTS vacina_aplicad(id INTEGER PRIMARY KEY AUTOINCREMENT, usuario INTEGER, vacina INTEGER, data TEXT);"
 
 def criartabela(conexao,sql):
     try:
